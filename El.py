25f160ddf3cddbab91fc64c8aabc2d5a77872402
Anastasiia Babenko <radiowavezes@gmail.com>
2022-08-19 17:59:16 +0300

    test

--- El.py
+++ El.py
@@ -1,16 +1,12 @@
-employee_list = []
-while True:
-    name = input('Enter the name: ')
-    salary = int(input('Enter the salary: '))
-    employee_list.append(name)
-    employee_list.append(salary)
-    do_continue = input('Do you want to continue? y/n ')
-    if do_continue == 'n':
-        break
-max_salary = 0
-for i in range(1, len(employee_list), 2):
-    if employee_list[i] > max_salary:
-        max_salary = employee_list[i]
-for i in range(1, len(employee_list), 2):
-    if employee_list[i] == max_salary:
-        print(employee_list[i - 1], '-', employee_list[i], end='', sep=' ')
+def average_num(myList):
+    s = 0
+    a = 0
+    for i in myList:
+        s += i
+        a += 1
+    return s / a
+
+
+
+myList = list(map(int, input().split()))
+print(average_num(myList))

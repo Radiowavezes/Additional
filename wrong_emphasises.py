f3be50929fd1d2787f60f917b17158eecc45595f
Anastasiia Babenko <radiowavezes@gmail.com>
2022-08-22 15:22:28 +0300

    -

--- wrong_emphasises.py
+++ wrong_emphasises.py
@@ -8,9 +8,10 @@ for word in inFile.readline().split():
     user_dict.append(word)
 inFile.close()
 emphas_dict_lower = [word.lower() for word in emphas_dict]
+user_dict_lower = [word.lower() for word in user_dict]
 mistakes = 0
-for user_word in zip(user_dict):
-    if (user_word.lower()) in emphas_dict_lower:
+for user_word, user_lower_word in zip(user_dict, user_dict_lower):
+    if user_lower_word in emphas_dict_lower:
         if user_word not in emphas_dict:
             mistakes += 1
     else:

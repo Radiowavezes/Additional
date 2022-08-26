88f5f90851e4daa9b9bd6c1669e2cad8112df9af
Radiowavezes <radiowavezes@gmail.com>
2022-08-26 23:24:04 +0300

    -

--- test.py
+++ test.py
@@ -56,5 +56,32 @@
 
 # print(print_tree(7))
 
+# import sys
 
-print(3^8)
+# randomList = ['a', 0, 2]
+
+# for entry in randomList:
+#     try:
+#         print("The entry is", entry)
+#         r = 10/int(entry)
+#         break
+#     except:
+#         print("Oops!", sys.exc_info()[0], "occurred.")
+#         print("Next entry.")
+#         print()
+# print("The reciprocal of", entry, "is", r)
+
+# import sys
+
+# randomList = ['a', 0, 2]
+
+# for entry in randomList:
+#     try:
+#         print("The entry is", entry)
+#         r = 1/int(entry)
+#         break
+#     except Exception as e:
+#         print("Oops!", e.__class__, "occurred.")
+#         print("Next entry.")
+#         print()
+# print("The reciprocal of", entry, "is", r)

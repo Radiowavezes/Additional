de0b9d342662e1df40e510ba3867323315807b64
Radiowavezes <radiowavezes@gmail.com>
2022-09-01 13:13:41 +0300

    -

--- intervals.py
+++ intervals.py
@@ -15,16 +15,16 @@ def sum_of_intervals(intervals):
         if reg == len(intervals):
             if len(result_list) == 0:
                 result_list.append((start, (max(intervals, key=lambda x: x[1]))[1]))
-                ammount = abs(result_list[0][1] - result_list[0][0])
+                ammount = (result_list[0][1] - result_list[0][0])
             else:
                 result_list.append(intervals[reg - 1])
                 for i in result_list:
-                    ammount += abs(i[1] - i[0])
+                    ammount += (i[1] - i[0])
     if reg == len(intervals):
-        ammount = abs(intervals[0][1] - intervals[0][0])
+        ammount = (intervals[0][1] - intervals[0][0])
 
     return ammount
 
 
-intervals = [(1, 5)]
+intervals = [(1, 4), (7, 10), (3, 5)]
 print(sum_of_intervals(intervals))
--- test.py
+++ test.py
@@ -1,20 +1,34 @@
-def increment_string(strng):
-    num_strng = ''
-    let_strng = ''
-    if strng[-1].isdigit() == False or len(strng) == 0:
-        strng += '1'
-    else:
-        for i in strng[::-1]:
-            if i.isdigit():
-                num_strng += i
+def sum_of_intervals(intervals):
+
+    uniq_intervals = set(intervals)
+    uniq_intervals = sorted(uniq_intervals, key=lambda x: x[0])
+    result_intervals = []
+    start = int(uniq_intervals[0][0])
+    stop = int(uniq_intervals[0][1])
+    counter = 0
+
+    for interval in uniq_intervals[1:]:
+        if int(interval[0]) not in range(start, stop):
+            result_intervals.append((start, stop))
+            start = int(interval[0])
+            if start == int(uniq_intervals[-1][0]):
+                stop = int(uniq_intervals[-1][1])
+                result_intervals.append((start, stop))
+        else:
+            stop = int(interval[1])
+    if len(result_intervals) > 0:
+        for start, stop in result_intervals:
+            if start < 0:
+                counter += (abs(start) + stop)
+            elif start < 0 and stop < 0:
+                counter += (abs(start) - (abs(stop)))
             else:
-                let_strng += i
-    number = int(num_strng[::-1])
-    if number > 100:
-        strng = let_strng[::-1] + '100'
+                counter += (stop - start)
     else:
-        strng = let_strng[::-1] + str(number + 1)
-    return strng    
+        counter += stop - start
+    
+    return counter
 
 
-print(increment_string("foobar099"))
+intervals = [(1, 4)]
+print(sum_of_intervals(intervals))

30d717a07b9635d5121ffeddcc5abd4c40656732
Anastasiia Babenko <radiowavezes@gmail.com>
2022-09-22 21:06:42 +0300

    -

---
+++ example.py
@@ -0,0 +1,2 @@
+def full_name(first_name, second_name):
+    return first_name + second_name
---
+++ test_example.py
@@ -0,0 +1,11 @@
+from example import full_name
+
+# class TestSomething:
+#     def test_somethng(self):
+#         pass
+
+def test_example():
+    first_name = 'John'
+    second_name = 'Smith'
+
+    assert full_name(first_name, second_name) == 'JohnSmith'

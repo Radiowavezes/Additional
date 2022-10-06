f8166368a51dcdbad031db5240402d0bc0446304
Anastasiia Babenko <radiowavezes@gmail.com>
2022-10-06 13:53:01 +0300

    -

--- Weight for weight.py
+++ Weight for weight.py
@@ -4,9 +4,8 @@ def order_weight(strng):
         for symb in str(x):
             res += int(symb)
         return res
-    new_strng = list(enumerate(map(int, strng.split())))
-    new_strng = sorted(new_strng, key = lambda x: sorting_sum(x[1]), reverse=True)
-    return ' '.join(str(i[1]) for i in new_strng[::-1])
+    new_strng = sorted(strng.split(), key = lambda x: sorting_sum(x))
+    return ' '.join(str(i) for i in new_strng)
 
 
 strng = "2000 10003 1234000 44444444 9999 11 11 22 123"

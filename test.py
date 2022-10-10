366c1ca8f896a1734e14c3e0c26ac7cecac47e86
Anastasiia Babenko <radiowavezes@gmail.com>
2022-10-10 17:25:11 +0300

    -

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-def to_camel_case(text):
-    for symb in '_-':
-        while symb in text:
-            text = text.replace(symb, ' ')
-    new_text1 = text.split()
-    new_text2 = list(word.capitalize() for word in new_text1[1:])
-    return new_text1[0] + ''.join(new_text2)
+def abbrev_name(name):
+    return f'{name.upper().split()[0][0]}.{name.split()[1][0]}'
 
-print(to_camel_case('the_stealth_warrior'))
-print(to_camel_case("The-Stealth-Warrior"))
+print(abbrev_name("Sam Harris"))

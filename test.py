b5e7ecae8f8ce1734fdffaa6b7d46ba20f04a6a1
Anastasiia Babenko <radiowavezes@gmail.com>
2022-10-20 15:03:19 +0300

    -

--- test.py
+++ test.py
@@ -1,33 +1,10 @@
-def longestPalindrome(s: str) -> str:
-    def is_palindrome(looking_str: str, index: int = 0) -> bool:
-        if int(len(looking_str) / 2) == index:
-            return True
-        if int(len(looking_str) / 2 + 1) == index:
-            return True
-        if looking_str[index] != looking_str[-(index + 1)]:
-            return False
-        return is_palindrome(looking_str, index + 1)
-    start = 0
-    stop = 1
-    recursive_string = ''
-    temp = ''
-    if len(s) > 1:
-        while start < len(s) - 1:
-            if is_palindrome(s[start:stop]):
-                temp = s[start:stop]
-                print(s[start:stop])
-                print(stop)
-                print(temp)
-            if len(temp) > len(recursive_string):
-                recursive_string = temp
-            stop += 1
-            if stop == len(s):
-                start += 1
-                stop = start + 1
-    else:
-        recursive_string = s
-    return recursive_string
+def binary_search(target:int, start:int=0, stop:int=100) -> int:
+    middle = (start + stop) // 2
+    if middle == target:
+        return f'the target was {middle}'
+    if middle > target:
+        return binary_search(target, start, middle - 1)
+    if middle < target:
+        return binary_search(target, middle + 1, stop)
 
-s = 'aabb'
-print(longestPalindrome(s))
-print(s[3::-1])
+print(binary_search(33))

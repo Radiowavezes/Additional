0e7f92997ae5c9aebc4e9028dfbfee10e0db5d4b
Radiowavezes <radiowavezes@gmail.com>
2022-10-24 20:54:26 +0300

    -

--- Orderlist.py
+++ Orderlist.py
@@ -1,4 +1,19 @@
-from node import Node
+class Node:
+    def __init__(self, data):
+        self._data = data
+        self._next = None
+
+    def get_data(self):
+        return self._data
+
+    def get_next(self):
+        return self._next
+
+    def set_data(self, data):
+        self._data = data
+
+    def set_next(self, new_next):
+        self._next = new_next
 
 
 class OrderedList:
